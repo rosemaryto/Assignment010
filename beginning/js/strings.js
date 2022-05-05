2ea969da5c0f8c09ba3053ab59c1e8982a10ae44
rosemaryto <rosemary_to@ymail.com>
2022-05-05 06:33:37 -0700

    start

---
+++ beginning/js/strings.js
@@ -0,0 +1,34 @@
+//STEP 1
+// let urName = prompt("What is your name")
+// alert(`${urName.length}`)
+
+//STEP 2
+// let urName = prompt("What is your name?")
+// let urNum = parseInt(prompt("Enter a number to get a letter in your name"))
+// urNum = urNum - 1
+// alert(`${urName.charAt(urNum)}`)
+
+
+//STEP 3
+// let firName = prompt("What is your first name?")
+// let lasName = prompt("What is your last name?")
+// alert(`Your name is: ${firName.concat(lasName)}`)
+
+//STEP 4
+// let txtApp = 'The quick brown fox jumps over the lazy dog'
+
+
+//STEP 5
+
+//STEP 6
+let txtApp = 'The quick brown fox jumps over the lazy dog'
+let urName = prompt("What is your name?")
+alert(`${txtApp.replace('the lazy dog', urName)}`)
+
+//STEP 7
+
+//STEP 8
+
+//STEP 9
+
+//STEP 10
